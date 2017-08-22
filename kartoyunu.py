f64b0a9a6ce44bd5de439b278a068f1fb624bf17
burakkdemir <burak.demirr@hotmail.com>
2017-08-22 21:20:09 +0300

    Add files via upload

---
+++ kartoyunu.py
@@ -0,0 +1,22 @@
+import random
+
+simgeler = ["Karo","Maça","Sinek","Kupa"]
+sayilar = [1,2,3,4,5,6,7,8,9,10,"Joker","Kız","Papaz"]
+kagitlar=[]
+
+for simge in simgeler:
+    for sayi in sayilar:
+        kagitlar.append(simge + " " + str(sayi) )
+
+oyuncular={}
+
+for oyuncu in range(1,5):
+    oyuncular["Oyuncu " + str(oyuncu)] = []
+    for kart in range(1,4):
+        kagit = kagitlar.pop(kagitlar.index(random.choice(kagitlar)))
+        oyuncular["Oyuncu "+ str(oyuncu)].append(kagit)
+
+for i in sorted(oyuncular):
+    print (i + "\n" + "--------")
+    for k in oyuncular[i]:
+        print (k)
